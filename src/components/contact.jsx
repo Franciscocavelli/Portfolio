8fc43ffddeb25e4c59429046c8b0f697213f3de1
BuildTools <unconfigured@null.spigotmc.org>
2023-05-03 16:59:41 -0300

    mailto

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -10,7 +10,7 @@ const Contact = (promp) => {
       <p className="contact__text">
         {promp.contacttext1}<br/><br/> {promp.contacttext2}<br/><br/> {promp.contacttext3}
       </p>
-      <a className="contact__buttonHi"><button>{promp.contactgreetme}</button></a>
+      <a href="mailto:francavelli@hotmail.com?Subject=Hola%20quisiera%20contactar%20contigo." className="contact__buttonHi"><button>{promp.contactgreetme}</button></a>
       <div className="contact__social">
         <a href="https://www.linkedin.com/in/francisco-tomas-cavelli-montini-7841a1184/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
         <a href="https://www.instagram.com/francavelli/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
