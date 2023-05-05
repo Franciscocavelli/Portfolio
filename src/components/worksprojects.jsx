e63ab0c22644181e9f390363649277571e701dc3
BuildTools <unconfigured@null.spigotmc.org>
2023-05-05 00:38:40 -0300

    fixes

--- src/components/worksprojects.jsx
+++ src/components/worksprojects.jsx
@@ -11,7 +11,9 @@ const Worksprojects = (promp) => {
       {promp.projectsworksSubTitle}
       </p>
       <div className="worksprojects__project">
-        <img src={app1} alt="App1" />
+        <div className="worksprojects__project__img">
+          <img src={app1} alt="App1" />
+        </div>
         <div className="worksprojects__projectText">
           <p className="worksprojects__projectText__Title">{promp.projectsworksApp1Title}</p>
           <p className="worksprojects__projectText__Leng">PHP - MYSQL - JAVASCRIPT - JQUERY - AJAX</p>
@@ -20,18 +22,12 @@ const Worksprojects = (promp) => {
       </div>
       <div className="worksprojects__projectIzq">
         <div className="worksprojects__projectText">
-          <p className="worksprojects__projectText__Title">{promp.projectsworksApp3Title}</p>
-          <p className="worksprojects__projectText__Leng">JAVASCRIPT - REACT</p>
-          <p className="worksprojects__projectText__Desc">{promp.projectsworksApp3Desc}</p>
-        </div>
-        <img src={app3} alt="App3" />
-      </div>
-      <div className="worksprojects__project">
-        <img src={app2} alt="App2" />
-        <div className="worksprojects__projectText">
           <p className="worksprojects__projectText__Title">{promp.projectsworksApp2Title}</p>
           <p className="worksprojects__projectText__Leng">JAVASCRIPT - REACT - PHP - MYSQL - LARAVEL<br/><b>API'S: </b>STEAM - MERCADO PAGO</p>
           <p className="worksprojects__projectText__Desc">{promp.projectsworksApp2Desc}</p>
+        </div>
+        <div className="worksprojects__project__img">
+          <img src={app2} alt="App2" />
         </div>
       </div>
     </div>
